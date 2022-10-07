e8eabbb3c5a8e1beb66ef88ac0d3033986136213
sassa9404 <safae.lachgar@sncf.fr>
2022-10-07 14:31:40 +0200

    01 - Modif

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+const message :string ='Hello world';
+const handleClick = (name: string) => alert(`Bonjour ${name}`);
+const handleClickSanParam = () => alert("yes Papa !");
+return (
+  <div>
+    <p className='color-pink'>{message}</p>
+    <button onClick={handleClickSanParam}>Test</button>
+    <p>coucou</p>
+    <button onClick={() => handleClick('Safae')}> Hello</button>
+  </div>
+);
+
+
+  // return <div>Hello word</div>;
+};
 
 export default App;
